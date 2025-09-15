26973daf8c5ea2c4fb75158b5c8bcde8577e8403
talha-388204 <monirulhasantalha@gmail.com>
2025-09-15 20:54:51 +0600

    Update firebase-config-sample.js

--- firebase-config-sample.js
+++ firebase-config-sample.js
@@ -1,15 +1,15 @@
 // firebase-config-sample.js
 // Replace the placeholders below with your Firebase web app config
 // How to get these: Firebase Console -> Project -> Settings -> SDK setup and config
-window.__FIREBASE_CONFIG = {
+window.__const firebaseConfig = {
   apiKey: "AIzaSyD4O6SHrpeDnzKMBWkudVFk1j1JoYiO3HY",
   authDomain: "calling-app-e7ad2.firebaseapp.com",
   projectId: "calling-app-e7ad2",
-  storageBucket: "storageBucket: "calling-app-e7ad2.appspot.com",",
+  storageBucket: "calling-app-e7ad2.firebasestorage.app",
   messagingSenderId: "241816266208",
   appId: "1:241816266208:web:3648364051e54f1ca83785"
 };
-
 // IMPORTANT: do not commit real keys to public repos in production. This demo is for learning only.
+
 
 
